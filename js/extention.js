569f06b189abbf397ea355d5f90b1fa0517b3190
asukakk <98793446+asukakk@users.noreply.github.com>
2022-06-14 21:00:21 +0900

    Add files via upload

---
+++ js/extention.js
@@ -0,0 +1,56 @@
+$(function(){
+	$('.slider').slick({
+		autoplay: true,
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerMode: true,
+        centerPadding: "15%",
+		responsive: [//レスポンシブ指定（指定したbreakpointによってオプションを上書き可能）
+			{
+				breakpoint: 600,//breakpoint
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,//breakpoint
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+});
+
+$(function(){
+	$('.slider-news').slick({
+		autoplay: true,
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [//レスポンシブ指定（指定したbreakpointによってオプションを上書き可能）
+			{
+				breakpoint: 600,//breakpoint
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 480,//breakpoint
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+});
+
